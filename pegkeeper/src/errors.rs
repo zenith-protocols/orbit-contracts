dfdb5ea45a613ce757ce5268a63d7bdfa2c2a7d0
Robeartt <olthuisrobin@gmail.com>
2025-11-13 12:25:50 +0100

    V2 wasms and removing pegkeeper

--- pegkeeper/src/errors.rs
+++
@@ -1,9 +0,0 @@
-use soroban_sdk::{self, contracterror};
-
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
-pub enum PegkeeperError {
-    NotProfitable = 1505,
-    PositionStillOpen = 1507,
-}
